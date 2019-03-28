00c6db12d6f7d902ca1a9ed186a5e43f96e5245d
Kim Tien <47434246+ktngn@users.noreply.github.com>
2019-03-28 09:39:03 -0500

    new class to analyze info

---
+++ HealthAnalyzer/src/Analyzer.java
@@ -0,0 +1,29 @@
+
+public class Analyzer extends GatherInfo {
+	private double bmi;
+	
+	public double calculateBMI(double w, double h) {
+		this.bmi = (w / (h * h)) * 705; // in inches
+		return bmi;
+	}
+	
+	public String analyzer() {
+		String statement = "";
+		if (this.bmi <= 19) {
+			statement = "You're considered underweight";
+		}
+		else if (this.bmi >19 && this.bmi <25 )
+		{
+			statement = "You're in the healthy range";
+		}
+		else if (this.bmi >= 25 && this.bmi <30) {
+			statement = "You're considered overweight";
+		}
+		else
+		{
+			statement = "You're considered obese";
+		}
+		return statement;
+	}
+	
+}
