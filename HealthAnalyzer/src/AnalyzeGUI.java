71831c543761f12fd66fa5431b70e40ee93601fb
Kim Tien <47434246+ktngn@users.noreply.github.com>
2019-04-18 17:58:36 -0500

    new line

--- HealthAnalyzer/src/AnalyzeGUI.java
+++ HealthAnalyzer/src/AnalyzeGUI.java
@@ -50,6 +50,7 @@ public class AnalyzeGUI extends JFrame implements ActionListener{
 	private JButton resetBtn = new JButton("Reset");
 	private JButton enterBtn = new JButton("Enter");
 	
+	
 	//Textfields
 	private JTextField firstNameTxt = new JTextField(10);
 	private JTextField lastNameTxt = new JTextField(10);
